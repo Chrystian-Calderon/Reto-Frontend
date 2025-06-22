b384d22a4b0b33b74ba9199137204171de1d6476
ralf <chrystianralfc@gmail.com>
2025-06-22 16:03:48 -0400

    feat: added screens and subscreens for equipos

--- Navigation.js
+++ Navigation.js
@@ -2,23 +2,51 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 import HomeScreen from "./screens/HomeScreen";
 import EquipoScreen from "./screens/EquipoScreen";
 import JugadoresScreen from "./screens/JugadoresScreen";
 import HistorialScreen from "./screens/HistorialScreen";
+import EquiposScreen from "./screens/EquiposScreen";
+import AddEquipoScreen from "./screens/AddEquipoScreen";
+import PartidosScreen from "./screens/PartidosScreen";
 
 const StackHome = createNativeStackNavigator();
+const StackEquipos = createNativeStackNavigator();
 const Tabs = createBottomTabNavigator();
 
 function HomeStack() {
   return (
     <StackHome.Navigator initialRouteName="HomeScreen">
-      <StackHome.Screen name="HomeScreen" component={HomeScreen} />
+      <StackHome.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          headerShown: false
+        }}
+      />
       <StackHome.Screen name="Equipo" component={EquipoScreen} />
       <StackHome.Screen name="Historial" component={HistorialScreen} />
     </StackHome.Navigator>
-  )
+  );
+}
+
+function EquiposStack() {
+  return (
+    <StackEquipos.Navigator initialRouteName="EquiposScreen">
+      <StackEquipos.Screen
+        name="EquiposScreen"
+        component={EquiposScreen}
+        options={{
+          headerShown: false
+        }}
+      />
+      <StackEquipos.Screen name="Equipo" component={EquipoScreen} />
+      <StackEquipos.Screen name="AgregarEquipo" component={AddEquipoScreen} />
+    </StackEquipos.Navigator>
+  );
 }
 
 function BottomTabs() {
@@ -36,8 +64,35 @@ function BottomTabs() {
         }}
       />
       <Tabs.Screen
+        name="Equipos"
+        component={EquiposStack}
+        options={{
+          tabBarLabel: 'Equipos',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="team" size={size} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
         name="Jugadores"
         component={JugadoresScreen}
+        options={{
+          tabBarLabel: 'Jugadores',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name="person-running" size={size} color={color} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="Partidos"
+        component={PartidosScreen}
+        options={{
+          tabBarLabel: 'Partidos',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="soccer" size={size} color={color} />
+          )
+        }}
       />
     </Tabs.Navigator>
   )
